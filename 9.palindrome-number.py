c5f40e3ffa9b520840e5192e6fc563a60354f536
FangtaiDong10 <106503583+FangtaiDong10@users.noreply.github.com>
2022-09-03 18:32:40 +1000

    getting rid of odd number

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -13,11 +13,13 @@ class Solution:
             return True
         if x % 10 == 0 and x != 0:
             return False
-            
+
         rev = 0
         while x > rev:
             rev = rev * 10 + x % 10
             x = x // 10
+
+        # when the length is an odd number, we can get rid of the middle digit by
         return x == rev or x == rev // 10
 # @lc code=end
 
